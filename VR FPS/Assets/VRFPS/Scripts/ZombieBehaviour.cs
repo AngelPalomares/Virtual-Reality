37bb3d7e38b21ca4ca76a3f8068f5a0fb84e1899
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2020-11-29 16:51:37 -0600

    implemented zombie

---
+++ VR FPS/Assets/VRFPS/Scripts/ZombieBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieBehaviour : MonoBehaviour
+{
+    private int currentHealth;
+    public int health = 100;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        currentHealth = health;
+
+        anim = GetComponent<Animator>();
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (currentHealth > 0)
+        {
+            TakeDamage(Random.Range(5, 20));
+        }
+    }
+
+    public void TakeDamage(int DamageType)
+    {
+        currentHealth -= DamageType;
+        if(currentHealth <= 0)
+        {
+            anim.SetBool("Dead", true);
+            new WaitForSeconds(2);
+            Destroy(gameObject);
+        }
+        else
+        {
+            anim.SetTrigger("Hit");
+        }
+    }
+
+}
