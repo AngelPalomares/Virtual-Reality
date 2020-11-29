38c13ebd01d29e2d02785bc3c8ca527ff11d1e75
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2020-11-28 20:16:49 -0600

    Update

---
+++ VR FPS/Assets/VRFPS/Scripts/HandGun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Examples;
+public class HandGun : GunShoot
+{
+    public Animator anim;
+    public AudioClip shootSound;
+    protected override void FireProjectile()
+    {
+        base.FireProjectile();
+
+        anim.SetTrigger("Fire");
+        AudioSource.PlayClipAtPoint(shootSound, transform.position);
+
+        //Debug.Log("Handgun Shot");
+    }
+}
