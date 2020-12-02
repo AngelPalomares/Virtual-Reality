940bc7822818c6316d885e675ac0e6872d50547a
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2020-12-02 13:56:43 -0600

    Update
    Added Zombie to game as well as added weapon mechanics to the game

---
+++ VR FPS/Assets/VRFPS/Scripts/PlayerBehariour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerBehariour : MonoBehaviour
+{
+    public int health;
+    private int currenthealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currenthealth = health;
+    }
+
+    void TakeDamage(int damagetodeal)
+    {
+        currenthealth -= damagetodeal;
+
+        if(currenthealth <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
